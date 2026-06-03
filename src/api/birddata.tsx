fcb12f1ff9bb168368c4d4fbc0f3c86efe2ea9c2
Sean Murtagh <purelolage@googlemail.com>
2026-06-03 23:27:43 +0100

    Robin added as another example

--- src/api/birddata.tsx
+++ src/api/birddata.tsx
@@ -4,6 +4,12 @@ const birddata = [
         summary: `A  bird that loves to hide from me`,
         images: ["1", "2", "3"],
         date: `1/12/2026`,
+    },
+    {
+        name: `Robin`,
+        summary: `One of my favourite birds to photograph. They sing all year round so we often associate them with Winter due to their red breast matching the post office delivery staff. But they are quite aggressive.`,
+        images: ["1", "2", "3"],
+        date: `30/05/2025`,
     }
 ]
 
