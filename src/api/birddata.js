32d407a0bde4a8ad42c957ad5af2e0d49fc0f301
Sean Murtagh <purelolage@googlemail.com>
2026-06-03 20:07:59 +0100

    init commit

---
+++ src/api/birddata.js
@@ -0,0 +1,10 @@
+const birddata = [
+    {
+        name: `Blackcap`,
+        summary: `A  bird that loves to hide from me`,
+        images: ["1", "2,", "3"],
+        date: `1/12/2026`,
+    }
+]
+
+export default birddata;
