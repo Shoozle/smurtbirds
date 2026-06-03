0a8a7bd2c667da0e23a44b773cc9e631126dc422
Sean Murtagh <purelolage@googlemail.com>
2026-06-03 21:28:25 +0100

    birddata api, render one bird, gallery

--- src/Bird.tsx
+++ src/Bird.tsx
@@ -1,7 +1,20 @@
-const Bird = () => {
+interface BirdProps {
+    name: string;
+    summary: string;
+    images: string[];
+    date: string;
+}
+
+function Bird({ name, summary, images, date }: BirdProps) {
     return (
-        <>
-        </>
+        <a>
+            <div id="wrapper" className="h-300 overflow-hidden">
+                <h2 className="text-2xl font-bold">{name}</h2>
+                <p>{summary}</p>
+                <p>{date}</p>
+                <img className="" src={images[0]} alt={name} />
+            </div>
+        </a>
     )
 }
 
--- src/Gallery.tsx
+++ src/Gallery.tsx
@@ -1,9 +1,16 @@
+import Bird from "./Bird";
+import birddata from "./api/birddata";
 
+const allBirds = birddata.map((bird) => (
+    <Bird name={bird.name} summary={bird.summary} images={bird.images} date={bird.date} />
+))
 
 const Gallery = () => {
+
     return (
-        <>
-        </>
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 text-gray-500">
+            {allBirds}
+        </div>
     )
 }
 
