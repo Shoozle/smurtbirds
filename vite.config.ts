e8ae14d3bd3a85d745720cd277d575bb762f7b5d
Sean Murtagh <purelolage@googlemail.com>
2026-06-04 00:17:16 +0100

    works on preview but not github why

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './smurtbirds',
+  base: '/smurtbirds/',
   plugins: [
     react(),
     tailwindcss()
