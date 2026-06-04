5842d35209158930a3c1d5a5b0fffd137cac3057
Sean Murtagh <purelolage@googlemail.com>
2026-06-04 20:35:24 +0100

    Buttons dynamically added for gallery if end/start

--- src/Bird.tsx
+++ src/Bird.tsx
@@ -6,16 +6,16 @@ interface BirdProps {
 
 function Bird({ onClick, name, images }: BirdProps) {
 
-    //const localurl = `Birds/thumbs/${name}${images[0]}.JPG`;
-    const githuburl = `https://shoozle.github.io/smurtbirds/Birds/thumbs/${name}${images[0]}.JPG`;
+    const localurl = `Birds/thumbs/${name}${images[0]}.JPG`;
+    //const githuburl = `https://shoozle.github.io/smurtbirds/Birds/thumbs/${name}${images[0]}.JPG`;
 
     return (
 
-        <div onClick={onClick} id="wrapper" className="rounded-lg shadow-lg border-2 overflow-hidden">
+        <div onClick={onClick} id="wrapper" className="rounded-lg shadow-lg border-2 overflow-hidden cursor-zoom-in hover:shadow-2xl">
             {/* <h2 className="text-2xl font-bold">{name}</h2>
                 <p>{summary}</p>
                 <p>{date}</p> */}
-            <img className="brightness-90 hover:brightness-110 hover:scale-110 transition ease-in duration-300" src={githuburl} alt={name} />
+            <img className="brightness-90 hover:brightness-110 hover:scale-110 transition ease-in duration-300" src={localurl} alt={name} />
         </div>
 
     )
--- src/Carousel.tsx
+++ src/Carousel.tsx
@@ -12,7 +12,7 @@ const Carousel = ({ name, images, activeImage, setActiveImage }: CarouselProps) 
     const [loaded, setLoaded] = useState(false);
 
     const loadingSpinner = (
-        <div className="absolute grid grid-cols-1 gap-4 text-white rounded-lg p-4 max-w-8xl bg-gray-800/90 mx-auto top-1/2 transform -translate-y-1/2">
+        <div className="absolute grid grid-cols-1 gap-4 text-white rounded-lg p-4 max-w-8xl bg-gray-800/90 mx-auto top-1/2 left-1/2 transform -translate-y-1/2 translate-x-1/2">
             <div className="col-span-1">
                 <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-gray-900"></div>
             </div>
@@ -23,25 +23,29 @@ const Carousel = ({ name, images, activeImage, setActiveImage }: CarouselProps) 
         <div>
             <div className="h-full">
                 <div className="relative rounded-base">
-                    <button
-                        className="absolute top-1/2 left-0 transform -translate-y-1/2 text-white focus:outline-none opacity-75"
-                        onClick={() => setActiveImage(Math.max(0, activeImage - 1))}>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-16">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="m11.25 9-3 3m0 0 3 3m-3-3h7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-                        </svg>
-                    </button>
+                    {activeImage > 0 && (
+                        <button
+                            className="absolute top-1/2 left-0 transform -translate-y-1/2 text-white focus:outline-none opacity-75 cursor-pointer hover:opacity-100 h-full
+                            bg-gradient-to-r from-gray-900/60 to-transparent"
+                            onClick={() => setActiveImage(Math.max(0, activeImage - 1))}>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-16">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="m11.25 9-3 3m0 0 3 3m-3-3h7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                            </svg>
+                        </button>)}
                     {!loaded && loadingSpinner}
-                    <img src={`Birds/${name}${images[activeImage]}.JPG`} alt={name} onLoad={() => setLoaded(true)} />
-                    <button
-                        className="absolute top-1/2 right-0 transform -translate-y-1/2 text-white focus:outline-none opacity-75"
-                        onClick={() => setActiveImage(Math.min(images.length - 1, activeImage + 1))}>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-16">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="m12.75 15 3-3m0 0-3-3m3 3h-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-                        </svg>
-                    </button>
+                    <img src={`Birds/${name}${images[activeImage]}.JPG`} alt={name} onLoad={() => setLoaded(true)} className="shadow-xl" />
+                    {activeImage < images.length - 1 && (
+                        <button
+                            className="absolute top-1/2 right-0 transform -translate-y-1/2 text-white focus:outline-none opacity-75 cursor-pointer hover:opacity-100 h-full
+                            bg-gradient-to-l from-gray-900/60 to-transparent"
+                            onClick={() => setActiveImage(Math.min(images.length - 1, activeImage + 1))}>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-16">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="m12.75 15 3-3m0 0-3-3m3 3h-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                            </svg>
+                        </button>)}
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
 
