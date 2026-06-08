ae5fdf492803799e3c0f671c408a9f8013b0ebd5
Sean Murtagh <purelolage@googlemail.com>
2026-06-08 23:38:58 +0100

    Multiple images added, lightbox and bird panel scrolls

--- src/Birdpanel.tsx
+++ src/Birdpanel.tsx
@@ -18,14 +18,17 @@ function BirdPanel({ onClick, onKeyDown, name, summary, images, location, date }
 
     return (
         <>
-            <div onClick={onClick} onKeyDown={() => onKeyDown} className="z-2 fixed top-0 left-0 w-screen h-screen bg-gray-900/80 cursor-zoom-out backdrop-blur-sm p-4">
+
+            <div onClick={onClick} onKeyDown={() => onKeyDown} className="z-2 h-full w-full fixed top-0 left-0 bg-gray-900/80 cursor-zoom-out backdrop-blur-sm p-4">
 
             </div>
-            <div className="z-3 absolute top-1/2 left-1/2 mt-1/2 ml-1/2 -translate-y-1/2 -translate-x-1/2 grid sm:grid-cols-1 w-full md:grid-cols-1 lg:grid-cols-3 max-w-[1600px] bg-gray-900/90 backdrop-blur-sm rounded-lg">
+            {/* <button onClick={onClick} className="cursor-pointer p-4 bg-gray-900/80 text-2xl bold">X</button> */}
+            <div className="z-3 fixed top-1/2 left-1/2 mt-1/2 ml-1/2 -translate-y-1/2 -translate-x-1/2 grid sm:grid-cols-1 w-full md:grid-cols-1 lg:grid-cols-3 max-w-[1600px] bg-gray-900/90 backdrop-blur-sm rounded-lg">
                 <div className="sm:col-span-1 md:col-span-2">
                     <Carousel name={name} images={images} activeImage={activeImage} setActiveImage={SetActiveImage} />
                 </div>
-                <div className="order-last flex flex-col  max-w-6xl mx-auto text-white pb-8 pt-8 px-6">
+                <div className="order-last relative flex flex-col max-w-6xl mx-auto text-white pb-8 pt-8 px-6">
+
                     <h1 className="text-2xl mb-8 font-bold">{name}</h1>
                     <p className="text-lg mb-auto">{summary}</p>
                     <p className="text-lg">Favourite photo was taken in {location} on {date}</p>
