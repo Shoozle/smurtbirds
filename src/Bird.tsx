ccf38ad2bec25e8b5da3d88ed4bd3c09c67fff6a
Sean Murtagh <purelolage@googlemail.com>
2026-06-08 18:21:10 +0100

    CSS on bird panel improved

--- src/Bird.tsx
+++ src/Bird.tsx
@@ -11,7 +11,7 @@ function Bird({ onClick, name, images }: BirdProps) {
     return (
 
         <div onClick={onClick} className="relative rounded-lg shadow-lg border-2 overflow-hidden cursor-zoom-in hover:shadow-2xl">
-            <h2 className="absolute pointer-events-none text-2xl z-1 top-1/2 left-1/2 mt-1/2 ml-1/2 -translate-y-1/2 text-black/75 -translate-x-1/2 font-bold">{name}</h2>
+            <h2 className="absolute pointer-events-none text-2xl z-1 top-1/2 left-1/2 mt-1/2 ml-1/2 -translate-y-1/2 text-white/75 -translate-x-1/2 font-bold text-2xl text-shadow-lg/30">{name}</h2>
             <img className="brightness-90 hover:brightness-110 hover:scale-110 transition ease-in duration-300" src={localurl} alt={name} />
         </div>
 
