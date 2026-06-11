cc43a0c4b2e1c5be49fc44d77f32ac33bc84601d
Sean Murtagh <purelolage@googlemail.com>
2026-06-12 00:29:30 +0100

    Try lazy loading on thumbs improve perf

--- src/Bird.tsx
+++ src/Bird.tsx
@@ -12,7 +12,7 @@ function Bird({ onClick, name, images }: BirdProps) {
 
         <div onClick={onClick} className="relative rounded-lg shadow-lg border-2 overflow-hidden cursor-zoom-in hover:shadow-2xl">
             <h2 className="absolute pointer-events-none text-2xl z-1 text-white/90 w-full text-center font-bold text-3xl pt-1 text-shadow-lg/30">{name}</h2>
-            <img className="brightness-90 hover:brightness-110 hover:scale-110 transition ease-in duration-300 w-full h-full" src={localurl} alt={name} />
+            <img className="brightness-90 hover:brightness-110 hover:scale-110 transition ease-in duration-300 w-full h-full" src={localurl} alt={name} loading="lazy" />
         </div>
 
     )
