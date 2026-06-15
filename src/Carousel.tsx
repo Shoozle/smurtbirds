e5b131977e44c7bcd871f7574adbf6e1cc4cff60
Sean Murtagh <purelolage@googlemail.com>
2026-06-15 20:21:12 +0100

    Removed loaded state

--- src/Carousel.tsx
+++ src/Carousel.tsx
@@ -9,7 +9,7 @@ type CarouselProps = {
 
 const Carousel = ({ name, images, activeImage, setActiveImage }: CarouselProps) => {
 
-    const [loaded, setLoaded] = useState(false);
+    // const [loaded, setLoaded] = useState(false);
 
     return (
         <div>
@@ -24,7 +24,7 @@ const Carousel = ({ name, images, activeImage, setActiveImage }: CarouselProps) 
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
                             </svg>
                         </button>)}
-                    <img src={`Birds/${name}${images[activeImage]}.JPG`} loading="lazy" alt={name} onLoad={() => setLoaded(true)} className="w-full h-full z-5 h-full w-full" />
+                    <img src={`Birds/${name}${images[activeImage]}.JPG`} loading="lazy" alt={name} className="w-full h-full z-5 h-full w-full" />
                     {activeImage < images.length - 1 && (
                         <button
                             className="absolute top-1/2 right-0 transform -translate-y-1/2 text-white focus:outline-none opacity-75 cursor-pointer hover:opacity-100 h-full
